ab032e32c811853042ed1068a1f7c8d5a3f2c10b
Obedience Munkombwe <omunkombwe@bms3.etp.kit.edu>
2025-08-12 11:53:24 +0200

    Add my_HEPMC and my_sh directories with initial files

---
+++ my_sh/trackFinderFitter_bb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MODEL_PATH=/work/omunkombwe/k4RecTracker/build/Tracking/test/inputFiles/SimpleGatrIDEAv3o1.onnx
+
+XML_FILE=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml
+STEERING_FILE=Tracking/test/testTrackFinder/SteeringFile_IDEA_o1_v03.py
+TBETA=0.6
+TD=0.3
+
+HEPMC_INPUT=/work/omunkombwe/k4RecTracker/my_HEPMC/ee_z_bb100.hepmc
+
+curl -o $STEERING_FILE https://raw.githubusercontent.com/key4hep/k4geo/master/example/SteeringFile_IDEA_o1_v03.py
+
+ddsim --steeringFile $STEERING_FILE \
+      --compactFile  $XML_FILE \
+      --inputFile $HEPMC_INPUT \
+      --random.seed 42 \
+      --numberOfEvents 100 \
+      --outputFile output_rootFiles/ee_z_bb0.root 
+    
+k4run Tracking/test/testTrackFinder/runTestTrackFinder.py --inputFile output_rootFiles/ee_z_bb0.root --outputFile output_rootFiles/ee_z_bb1.root --modelPath $MODEL_PATH --tbeta $TBETA --td $TD
+
+k4run Tracking/test/testTrackFitter/runTestTrackFitter.py --inputFile output_rootFiles/ee_z_bb1.root --outputFile output_rootFiles/ee_z_bb2.root
